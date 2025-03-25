3eeda31a9f16ad13dec1e0d9ec7a0db15ada4014
Sujal Agrawal <agrawalsujal08@gmail.com>
2025-03-25 19:19:37 +0530

    update: mongodb connection

--- node-app/index.js
+++ node-app/index.js
@@ -1,51 +1,62 @@
-require('dotenv').config();
-const express = require('express')
-const cors = require('cors')
-const path = require('path');
-var jwt = require('jsonwebtoken');
-const multer = require('multer')
-const productController = require('./controllers/productController');
-const userController = require('./controllers/userController');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+var jwt = require("jsonwebtoken");
+const multer = require("multer");
+const productController = require("./controllers/productController");
+const userController = require("./controllers/userController");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
-const upload = multer({ storage: storage })
-const bodyParser = require('body-parser')
-const app = express()
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+const upload = multer({ storage: storage });
+const bodyParser = require("body-parser");
+const app = express();
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = 4000
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI)
+const port = 4000;
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to mongodb", error);
+  });
 
-app.get('/', (req, res) => {
-    res.send('hello...')
-})
+app.get("/", (req, res) => {
+  res.send("hello...");
+});
 
-app.get('/search', productController.search)
-app.post('/like-product', userController.likeProducts)
-app.post('/add-product', upload.fields([{ name: 'pimage' }, { name: 'pimage2' }]), productController.addProduct)
-app.get('/get-products', productController.getProducts)
-app.get('/get-product/:pId', productController.getProductsById)
-app.post('/liked-products', userController.likedProducts)
-app.post('/my-products', productController.myProducts)
-app.post('/signup', userController.signup)
-app.get('/my-profile/:userId', userController.myProfileById)
-app.get('/get-user/:uId', userController.getUserById)
-app.post('/login', userController.login)
+app.get("/search", productController.search);
+app.post("/like-product", userController.likeProducts);
+app.post(
+  "/add-product",
+  upload.fields([{ name: "pimage" }, { name: "pimage2" }]),
+  productController.addProduct
+);
+app.get("/get-products", productController.getProducts);
+app.get("/get-product/:pId", productController.getProductsById);
+app.post("/liked-products", userController.likedProducts);
+app.post("/my-products", productController.myProducts);
+app.post("/signup", userController.signup);
+app.get("/my-profile/:userId", userController.myProfileById);
+app.get("/get-user/:uId", userController.getUserById);
+app.post("/login", userController.login);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
